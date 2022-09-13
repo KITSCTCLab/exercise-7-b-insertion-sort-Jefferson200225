bd9e232ae608bbe7934c24ff93b61b6097f06e00
Jefferson200225 <110010433+Jefferson200225@users.noreply.github.com>
2022-09-13 11:32:32 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,9 +2,14 @@ from typing import List
 
 def insertionSort(array) -> List[int]:
   # Write your code here
-
-# data = [9, 5, 1, 4, 3]
-input_data = input()
+  for i in range(1,len(array)):
+    min = i
+    for j in range(i-1,-1,-1):
+      if (array[j] > array[min]):
+        array[j],array[min] = array[min],array[j]
+        min -=1
+  return array
+# data = [9, 5, 1, 4, 3
 data = []
 for item in input_data.split(', '):
   if item.isnumeric():
